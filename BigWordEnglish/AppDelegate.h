2cfd5272b2d6d5f491042695fbd156b3892e9bd1
심규용 <SKY@simgyuyongui-MacBook-Pro.local>
2017-05-16 13:35:54 +0900

    최초 등록

---
+++ BigWordEnglish/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BigWordEnglish
+//
+//  Created by Joseph_iMac on 2017. 5. 9..
+//  Copyright © 2017년 Joseph_iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
